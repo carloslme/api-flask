dab1ffd9eb1582d4fa05d3ebd9cc6402d94c9ca4
carloslme <32799711+carloslme@users.noreply.github.com>
2021-01-12 00:24:44 -0600

    Script to simple Flask server, model with testing connection and csv.

---
+++ app.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 11 23:48:12 2021
+
+@author: carloslme
+"""
+
+import pandas as pd
+from flask import Flask, jsonify, request
+import pickle
+import requests
+import json
+
+# load model
+model = pickle.load(open('model.pkl','rb'))
+
+# app
+app = Flask(__name__)
+
+# routes
+@app.route('/', methods=['POST'])
+
+def predict():
+    # get data
+    data = request.get_json(force=True)
+
+    # convert data into dataframe
+    data.update((x, [y]) for x, y in data.items())
+    data_df = pd.DataFrame.from_dict(data)
+
+    # predictions
+    result = model.predict(data_df)
+
+    # send back to browser
+    output = {'results': int(result[0])}
+
+    # return data
+    return jsonify(results=output)
+
+if __name__ == '__main__':
+    app.run(port = 5000, debug=True)
+#@app.route('/', methods=['POST'])
